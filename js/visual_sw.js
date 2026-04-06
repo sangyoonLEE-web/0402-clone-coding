123aec4c7f8a7a55ac6f2dc669adde39ec2307e5
sangyoonLEE-web <abcbcd1540@gmail.com>
2026-04-06 17:54:55 +0900

    swiper적용, 마케팅부분완성

---
+++ js/visual_sw.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".visual-sw", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+});
